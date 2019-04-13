4b06fefb2cee2755e2c684862c3925b3368079d4
Weiss <DW046824@cerner.net>
2019-04-13 15:42:00 -0500

    Test

---
+++ FirstTest/script.js
@@ -0,0 +1,14 @@
+//
+// Blackjack
+//  Dylan
+//
+
+
+let card1 = "Ace of Spades";
+let card2 = "Ten of Hearts";
+
+console.log("Welcom to Blackjack!");
+
+console.log("You are Dealt: ");
+console.log(" "+ card1);
+console.log(" "+ card2);
